bc503237b9fb4409df48dcc2192ee464674a9521
KarbanLVA <iam82@ukr.net>
2022-07-21 12:50:13 +0300

    виконано завдання №2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const createGalleryItemMarkup = galleryItems.map(({preview, original, description}) => `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`).join("");
+galleryList.insertAdjacentHTML('beforeend',
+    createGalleryItemMarkup);
+
+galleryList.addEventListener('click', onGalleryImageClick);
+function onGalleryImageClick(event) {  
+    event.preventDefault();
+     if (!event.target.classList.contains('gallery__image')) {
+    return;
+     } 
+}
+    
+    let gallery = new SimpleLightbox(".gallery a", {
+captionsData: "alt",
+captionDelay: 250,
+});
